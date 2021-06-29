c6ef4ae9386772ac62a788bc0962e7e4ed2ded14
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-29 16:33:01 -0700

    feat: initial version (#1)
    * docs(README): remove WIP notice
    
    * feat: initial version

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = { getCurrentVersions };
+
+const fetch = require("node-fetch");
+
+const ENTERPRISE_VERSIONS_URL =
+  "https://raw.githubusercontent.com/github/docs/main/lib/enterprise-dates.json";
+
+async function getCurrentVersions() {
+  const response = await fetch(ENTERPRISE_VERSIONS_URL);
+  const data = await response.json();
+  const today = new Date().toISOString().substr(0, 10);
+
+  return Object.entries(data)
+    .filter(([, { releaseDate, deprecationDate }]) => {
+      if (releaseDate > today) return;
+      if (deprecationDate < today) return;
+
+      return true;
+    })
+    .map(([version]) => parseFloat(version))
+    .sort();
+}
